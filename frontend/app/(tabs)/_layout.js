ecb7996bd835189da11de24c758fa597c039c75a
Sanik Karmacharya <sanikkarmacharya1000@gmail.com>
2025-12-10 15:49:55 +0545

    login and signup page completed and push to sanik remote branch for the first time

--- frontend/app/(tabs)/_layout.js
+++ frontend/app/(tabs)/_layout.js
@@ -1,7 +1,16 @@
 import { Tabs } from "expo-router";
 import CustomTabBar from "../../components/navbar";
+import SignupScreen from "../../screens/SignupScreen";
+import LoginScreen from "../../screens/LoginScreen";
 
 export default function TabLayout() {
+  
+  const TEST_SIGNUP = true; // change to false later
+
+  if (TEST_SIGNUP) {
+    return <LoginScreen />;   // <--- ONLY SIGNUP SCREEN RENDERS
+  }
+
   return (
     <Tabs
       screenOptions={{ headerShown: false }}
