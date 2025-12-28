1e121c16d043f80ad03b32870f374da85dcf488f
Sanik Karmacharya <sanikkarmacharya1000@gmail.com>
2025-12-28 08:33:36 +0545

    membershipapproval and notification ui

--- admin-panel-frontend/src/App.jsx
+++ admin-panel-frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import { Route, Routes } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
+import MembershipApproval from './pages/MembershipApproval'
+import Notification from './pages/Notification'
 
 function App() {
   return (
+   
     <Routes>
+      <Route path='/membership' element={<MembershipApproval/>}/>
+      <Route path="/sendnotification" element={<Notification/>}/>
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   )
 }
 
-export default App
+export default App;
