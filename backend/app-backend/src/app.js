53b5579b2281a4ece38ecfab4422f2f6281b8c41
Lawan-Poudyal <poudyallawan@gmail.com>
2026-02-10 00:33:40 +0545

    added eventroutes

--- backend/app-backend/src/app.js
+++ backend/app-backend/src/app.js
@@ -1,12 +1,24 @@
 import cors from "cors";
 import express from "express";
-import profileRoutes from "./routes/profile.js";
+import logger from "./middleware/logger.js";
+import eventRoutes from "./routes/eventRoutes.js";
+import profileRoutes from "./routes/profileRoutes.js";
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+app.use(logger);
 
 app.use("/api/profile", profileRoutes);
+app.use("/api/events", eventRoutes);
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+});
 
 export default app;
