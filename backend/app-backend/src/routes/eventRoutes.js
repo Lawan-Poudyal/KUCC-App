dbdbb2849b881d5be71c9ad836c6709ee24ec332
Lawan-Poudyal <poudyallawan@gmail.com>
2026-02-12 23:39:14 +0545

    Feat: added routes to registerForEvent

--- backend/app-backend/src/routes/eventRoutes.js
+++ backend/app-backend/src/routes/eventRoutes.js
@@ -1,10 +1,21 @@
+// app-backend/src/routes/eventRoutes.js
+
 import express from "express";
-import { getAllPublishedEvents } from "../controllers/eventController.js";
+import {
+    getAllPublishedEvents,
+    getEventbyID,
+    registerForEvent,
+} from "../controllers/eventController.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 /* GET event */
-
 router.get("/", getAllPublishedEvents);
+// GET event by id
+router.get("/:id", getEventbyID);
+// POST /api/events/registe
+// @desc register for event
+router.post("/register", authMiddleware, registerForEvent);
 
 export default router;
