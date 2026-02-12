2314095ef53551436d3e2ad1da1b4e46f122dcb3
Lawan-Poudyal <poudyallawan@gmail.com>
2026-02-12 23:38:49 +0545

    Feat: Added fetchEventbyId and registerForEvent

--- frontend/services/eventsApi.tsx
+++ frontend/services/eventsApi.tsx
@@ -1,6 +1,8 @@
+// frontend/services/eventApi.tsx
+
 export const fetchEvents = async () => {
   try {
-    const res = await fetch(`http://192.168.16.104:3000/api/events`, {
+    const res = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/api/events`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -12,9 +14,63 @@ export const fetchEvents = async () => {
     }
     const data = await res.json();
 
+    // console.log(data.events);
+    // console.log(
+    //   "Statuses:",
+    //   data.events.map((e) => e.status),
+    // );
+
     return data.events || [];
   } catch (error) {
     console.error("Error fetching events:", error);
+    throw error;
+  }
+};
+
+export const fetchEventById = async (id) => {
+  try {
+    const res = await fetch(
+      `${process.env.EXPO_PUBLIC_API_URL}/api/events/${id}`,
+    );
+
+    if (!res.ok) {
+      throw new Error(`Failed to fetch event with id ${id}: ${res.status}`);
+    }
+
+    const data = await res.json();
+    return data.event;
+  } catch (error) {
+    console.error("Error fetching event by ID:", error);
+    return null;
+  }
+};
+
+export const registerForEvent = async (eventId, payment_method, token) => {
+  try {
+    const res = await fetch(
+      `${process.env.EXPO_PUBLIC_API_URL}/api/events/register`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          eventId,
+          payment_method,
+        }),
+      },
+    );
+
+    const data = await res.json();
+
+    if (!res.ok) {
+      throw new Error(data.message || "Registration failed");
+    }
+
+    return data;
+  } catch (error) {
+    // console.error("Register error:", error);
     throw error;
   }
 };
